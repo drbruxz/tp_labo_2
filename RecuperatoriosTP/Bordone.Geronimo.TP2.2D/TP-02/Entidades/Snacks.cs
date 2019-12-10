31242e0118cf956afacca56e6df57be977305103
drbruxz <48925335+drbruxz@users.noreply.github.com>
2019-12-10 11:15:48 -0300

    Add files via upload

---
+++ RecuperatoriosTP/Bordone.Geronimo.TP2.2D/TP-02/Entidades/Snacks.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades_2018
+{
+    public class Snacks : Producto
+    {
+        /// <summary>
+        /// Constructor de Snacks
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="codigo"></param>
+        /// <param name="color"></param>
+        public Snacks(EMarca marca, string codigo, ConsoleColor color) : base(codigo, marca, color)
+        {
+
+        }
+        /// <summary>
+        /// Los snacks tienen 104 calorías
+        /// </summary>
+        public override short CantidadCalorias
+        {
+            get
+            {
+                return 104;
+            }
+        }
+
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("SNACKS");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendFormat("CALORIAS : {0}\r\n", this.CantidadCalorias);
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
